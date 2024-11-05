bdc89e3950db86312b721bf971c404e659552b2f
rahmaaa <rahmashreifhassan@gmail.com>
2024-11-05 18:59:43 +0200

    Add files via upload

---
+++ my first c++ app.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string Name = "Rahma Sherif";
+	int Age = 19;
+	string city = "Cairo";
+	string country = "Egypt";
+	int  monthlysalary = 5000;
+	float yearlysalary = 60000;
+	char gender = 'F';
+	bool ismarried = false;
+	
+	cout << "********************************************************\n";
+
+	cout << "Name:" << Name << endl;
+	cout << "Age:" << Age << endl;
+	cout << "city:" << city << endl;
+	cout << "country:" << country << endl;
+	cout << "monthlysalary:" << monthlysalary << endl;
+	cout << "yearlysalary:" << yearlysalary << endl;
+	cout << "gender:" << gender << endl;
+	cout << "ismarried:" << ismarried << endl;
+
+
+	cout << "********************************************************\n";
+ 
+
+
+
+
+
+    
+
+	return 0;
+
+
+
+
+
+}
